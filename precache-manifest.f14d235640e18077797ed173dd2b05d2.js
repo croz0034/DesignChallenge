c68b1bb3fbbbb8d7876b634e3d9fb141a0535567
croz0034 <32398078+croz0034@users.noreply.github.com>
2019-08-16 01:21:15 -0400

    Updates

---
+++ precache-manifest.f14d235640e18077797ed173dd2b05d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64583b2968a6254818f3e649de0fbb7",
+    "url": "/DesignChallenge/index.html"
+  },
+  {
+    "revision": "8e7e76e1623ab9ffb3d1",
+    "url": "/DesignChallenge/static/css/main.7295ce61.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/DesignChallenge/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8e7e76e1623ab9ffb3d1",
+    "url": "/DesignChallenge/static/js/main.9f27a6a5.chunk.js"
+  },
+  {
+    "revision": "6cb8a30ca76b30c7bd41",
+    "url": "/DesignChallenge/static/js/runtime~main.c095d5b5.js"
+  },
+  {
+    "revision": "9ba3d853db4e4b6df296151320328b4a",
+    "url": "/DesignChallenge/static/media/BurgerBar.9ba3d853.svg"
+  },
+  {
+    "revision": "89266e87b76786e1cdff70c7a58c126f",
+    "url": "/DesignChallenge/static/media/Options.89266e87.svg"
+  }
+]);
